c0503214b34af9222ec9e2f2bdc54c8f3c30341e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-02-20 14:18:14 +0100

    write some gif frames

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use warp::Filter;
 use warp::hyper::Body;
+use warp::http::Response;
+use warp::http::status::StatusCode;
 
 fn handle() -> impl warp::Reply {
     let stream = tokio::stream::once(Ok::<_, Box<dyn std::error::Error + Send + Sync>>("bar"));
@@ -17,8 +19,47 @@ fn handle() -> impl warp::Reply {
         .body(Body::wrap_stream(stream))
 }
 
+fn image() -> impl warp::Reply {
+    let mut output = Vec::new();
+    {
+        let palette = &[0xff, 0xff, 0xff, 0, 0, 0];
+
+        let bitmap1 = [
+            0, 1,
+            1, 0
+        ];
+
+        let bitmap2 = [
+            1, 0,
+            0, 1,
+        ];
+
+        let mut encoder = gif::Encoder::new(&mut output, 2, 2, palette).expect("encoder");
+        use gif::SetParameter;
+        encoder.set(gif::Repeat::Infinite).expect("infinite");
+
+        let mut frame = gif::Frame::default();
+        frame.width = 2;
+        frame.height = 2;
+        frame.buffer = std::borrow::Cow::Borrowed(&bitmap1);
+        encoder.write_frame(&frame).expect("frame1");
+
+        let mut frame = gif::Frame::default();
+        frame.width = 2;
+        frame.height = 2;
+        frame.delay = 0;
+        frame.buffer = std::borrow::Cow::Borrowed(&bitmap2);
+        encoder.write_frame(&frame).expect("frame2");
+    }
+
+    Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "image/gif")
+        .body(output)
+}
+
 #[tokio::main]
 async fn main() {
-    let routes = warp::any().map(handle);
+    let routes = warp::any().map(image);
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
