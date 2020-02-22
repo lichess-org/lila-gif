fe07a701b2fc06aab02122ef8c088b7053c3fe37
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-02-22 01:08:40 +0100

    clean up api

--- src/api.rs
+++ src/api.rs
@@ -1,12 +1,11 @@
-use serde::Deserialize;
-use serde::de::Deserializer;
+use serde::{Deserialize, de};
 use serde_with::rust::display_fromstr;
 use shakmaty::Square;
 use shakmaty::fen::Fen;
 use shakmaty::uci::Uci;
 
 #[derive(Deserialize)]
-struct RequestBody {
+pub struct RequestBody {
     white: String,
     black: String,
     frames: Vec<RequestFrame>,
@@ -15,7 +14,7 @@ struct RequestBody {
 }
 
 #[derive(Deserialize)]
-struct RequestFrame {
+pub struct RequestFrame {
     #[serde(with = "display_fromstr")]
     f: Fen,
     #[serde(default)]
@@ -32,11 +31,11 @@ fn uci_null() -> Uci {
 
 fn maybe_square<'de, D>(deserializer: D) -> Result<Option<Square>, D::Error>
 where
-    D: Deserializer<'de>,
+    D: de::Deserializer<'de>,
 {
     Option::<&str>::deserialize(deserializer).and_then(|maybe_name| {
         Ok(match maybe_name {
-            Some(name) => Some(name.parse().map_err(|_| serde::de::Error::custom("invald square name"))?),
+            Some(name) => Some(name.parse().map_err(|_| de::Error::custom("invald square name"))?),
             None => None,
         })
     })
