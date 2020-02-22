60899ce37d725f0f826156952b5b9bae4450b56b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-02-22 16:21:22 +0100

    read sprite data

--- src/theme.rs
+++ src/theme.rs
@@ -1,21 +1,22 @@
 use gift::block::Preamble;
 use rusttype::FontCollection;
 use rusttype::Font;
+use ndarray::Array2;
 
 pub struct Theme {
     preamble: Preamble,
+    sprites: Array2<u8>,
     font: Font<'static>,
 }
 
 impl Theme {
     pub fn new() -> Theme {
-        println!("hello world");
         let theme_data = include_bytes!("../theme/theme.gif") as &[u8];
-        let mut decoder = gift::Decoder::new(std::io::Cursor::new(theme_data)).into_frame_decoder();
+        let mut decoder = gift::Decoder::new(std::io::Cursor::new(theme_data)).into_frames();
         let preamble = decoder.preamble().expect("decode preamble").expect("preamble");
-
-        println!("preamble: {:?}", preamble);
         let frame = decoder.next().expect("frame").expect("decode frame");
+        let sprites = Array2::from_shape_vec((720, 720), frame.image_data.data().to_owned()).expect("from shape");
+
         dbg!(frame.graphic_control_ext);
         dbg!(frame.image_data.data().len());
 
@@ -27,7 +28,12 @@ impl Theme {
 
         Theme {
             preamble,
+            sprites,
             font,
         }
+    }
+
+    pub fn background_color(&self) -> u8 {
+        self.sprites[(0, 0)]
     }
 }
