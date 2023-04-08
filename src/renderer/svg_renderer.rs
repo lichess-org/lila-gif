d3214966747a9c3ac9e6523b31a840b7004338cb
Filip Bachul <filipbachul@gmail.com>
2023-04-09 00:37:18 +0200

    reuse sprites

--- src/renderer/svg_renderer.rs
+++ src/renderer/svg_renderer.rs
@@ -1,7 +1,7 @@
 use std::{io::Write, vec};
 
 use bytes::{buf::Writer, BufMut, Bytes, BytesMut};
-use shakmaty::Bitboard;
+use shakmaty::{Bitboard, Color, Piece, Role};
 
 use super::renderer::{highlight_uci, RenderFrame, RenderState};
 use crate::{
@@ -45,7 +45,7 @@ impl Iterator for SVGRenderer {
         match self.state {
             RenderState::Preamble => {
                 output.write(svg_preamble.as_bytes()).unwrap();
-                render_defs(&mut output);
+                render_defs(&mut output, &self.theme);
 
                 let frame = self.frames.next().unwrap_or_default();
                 render_chessboard(&mut output, &frame, &self.theme, &self.orientation);
@@ -65,11 +65,33 @@ impl Iterator for SVGRenderer {
     }
 }
 
-fn render_defs(output: &mut Writer<BytesMut>) {
+fn render_defs(output: &mut Writer<BytesMut>, theme: &SvgTheme) {
     output.write("
     <defs>
-    <radialGradient id=\"check_gradient\" r=\"0.5\"><stop offset=\"0%\" stop-color=\"#ff0000\" stop-opacity=\"1.0\" /><stop offset=\"50%\" stop-color=\"#e70000\" stop-opacity=\"1.0\" /><stop offset=\"100%\" stop-color=\"#9e0000\" stop-opacity=\"0.0\" /></radialGradient>
-    </defs>".as_bytes()).unwrap();
+    <radialGradient id=\"check_gradient\" r=\"0.5\"><stop offset=\"0%\" stop-color=\"#ff0000\" stop-opacity=\"1.0\" /><stop offset=\"50%\" stop-color=\"#e70000\" stop-opacity=\"1.0\" /><stop offset=\"100%\" stop-color=\"#9e0000\" stop-opacity=\"0.0\" /></radialGradient>".as_bytes()).unwrap();
+    for piece_color in Color::ALL {
+        for piece_role in Role::ALL {
+            let piece = Piece {
+                color: piece_color,
+                role: piece_role,
+            };
+            let piece_sprite = theme.get_piece(piece);
+            let square_size = theme.square_size();
+            output
+                .write(
+                    format!(
+                        "<svg id=\"{}{}\" width=\"{square_size}\" height=\"{square_size}\">",
+                        piece.color.char(),
+                        piece.char()
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
+            output.write(piece_sprite).unwrap();
+            output.write("</svg>".as_bytes()).unwrap();
+        }
+    }
+    output.write("</defs>".as_bytes()).unwrap();
 }
 
 fn render_chessboard(
@@ -122,9 +144,9 @@ fn render_chessboard(
                 .write(
                     format!(
                         "<circle cx=\"{}\" cy=\"{}\" r=\"{}\" fill=\"url(#check_gradient)\" />",
-                        x + (square_size/2),
-                        (y +  square_size) / 2,
-                        square_size/2
+                        x + (square_size / 2),
+                        (y + square_size) / 2,
+                        square_size / 2
                     )
                     .as_bytes(),
                 )
@@ -142,19 +164,14 @@ fn render_pieces(
     for (sq, piece) in frame.board.clone() {
         let square_size = theme.square_size();
         println!("render pieces {sq} {:?}", piece);
-        let sprite = theme.get_piece(piece);
 
         let x = orientation.x(sq) * square_size;
         let y = orientation.y(sq) * square_size;
-        output
-            .write(
-                format!(
-                    "<svg x=\"{x}\" y=\"{y}\" width=\"{square_size}\" height=\"{square_size}\">"
-                )
-                .as_bytes(),
-            )
-            .unwrap();
-        output.write(sprite).unwrap();
-        output.write("</svg>".as_bytes()).unwrap();
+        let sprite = format!(
+            "<use href=\"#{}{}\" x=\"{x}\" y=\"{y}\" />",
+            piece.color.char(),
+            piece.char()
+        );
+        output.write(sprite.as_bytes()).unwrap();
     }
 }
